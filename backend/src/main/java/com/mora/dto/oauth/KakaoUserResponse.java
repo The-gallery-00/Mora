cce8d5b1eab688f2c07d952e6400a73172282e94
ChoiBeomsu <cvgy1915@naver.com>
2026-05-17 18:27:33 +0900

    fix: enable kakao oauth login

--- backend/src/main/java/com/mora/dto/oauth/KakaoUserResponse.java
+++ backend/src/main/java/com/mora/dto/oauth/KakaoUserResponse.java
@@ -1,6 +1,8 @@
 package com.mora.dto.oauth;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +12,12 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserResponse {
 
     private Long id;
     private JsonNode properties;
+
+    @JsonProperty("kakao_account")
     private JsonNode kakaoAccount;
 }
